1d4682c87dd0c2a3d69db625694c1b43fee94124
naomimunezero <munezeronaomi42@gmail.com>
2024-09-02 16:26:49 +0300

    making items dynamic

--- todolist/lib/screens/home.dart
+++ todolist/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:todolist/constants/colors.dart'; // Make sure to import your colors file
@@ -14,28 +15,53 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 25,vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50,bottom: 20),
-                    child: Text('All ToDos',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w500),),
-                ),
-
-                for (ToDo todoo in todosList)
-                  TodoItem(todo: todoo,),
-
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 25,vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50,bottom: 20),
+                        child: Text('All ToDos',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w500),),
+                    ),
+          
+                    for (ToDo todoo in todosList)
+                      TodoItem(todo: todoo,),
+          
+                  ],
+                )
+                )
+                // Add other widgets as needed
               ],
-            )
-            )
-            // Add other widgets as needed
-          ],
-        ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(child: Container(margin: EdgeInsets.only(bottom: 20,right: 20,left:20,),
+              padding: EdgeInsets.symmetric(horizontal: 20,vertical:5 ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [BoxShadow(color: Colors.grey,offset: Offset(0.0, 0.0),
+                blurRadius: 10.0,
+                spreadRadius: 0.0,),],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:TextField(
+                decoration: InputDecoration(
+                  hintText: "Add new ToDo item",
+                  border: InputBorder.none
+                )
+              )
+              ))
+            ],),
+          )
+        ],
       ),
     );
   }
--- todolist/lib/widget/todo_item.dart
+++ todolist/lib/widget/todo_item.dart
@@ -21,13 +21,13 @@ class TodoItem extends StatelessWidget {
           ),
         contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
         tileColor: Colors.white,
-        leading: Icon(Icons.check_box,color: tdBlue,),
+        leading: Icon(todo.isDone? Icons.check_box:Icons.check_box_outline_blank, color: tdBlue,),
         title: Text(
           todo.todoText!,
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
-            decoration: TextDecoration.lineThrough),
+            decoration: todo.isDone? TextDecoration.lineThrough : null),
             ),
         trailing: Container(
           padding: EdgeInsets.all(0),
